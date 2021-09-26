5b6320bbbd94bf39f445d47c1884a5831539dfe5
Denis Klemeshov <denis.klemeshov@gmail.com>
2021-09-27 01:14:33 +0300

    add: no banks placeholder

--- client/src/pages/banks/banks.jsx
+++ client/src/pages/banks/banks.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import BankCard from "../../components/bank-card/bank-card";
 import CreateBank from "../../components/create-bank/create-bank";
 
@@ -17,15 +18,21 @@ export default function BanksPage({ banks, fetchBanks }) {
     <Container>
       <CreateBank fetchBanks={fetchBanks} />
       <Grid container spacing={3}>
-        {banks.map((bank) => (
-          <Grid item xs={12} md={6} lg={4} key={bank.id}>
-            <BankCard
-              fetchBanks={fetchBanks}
-              bank={bank}
-              handleDelete={handleDelete}
-            />
-          </Grid>
-        ))}
+        {banks.length ? (
+          banks.map((bank) => (
+            <Grid item xs={12} md={6} lg={4} key={bank.id}>
+              <BankCard
+                fetchBanks={fetchBanks}
+                bank={bank}
+                handleDelete={handleDelete}
+              />
+            </Grid>
+          ))
+        ) : (
+          <Typography sx={{ color: "#434343" }} variant="h5" component="h2">
+            There are no banks...
+          </Typography>
+        )}
       </Grid>
     </Container>
   );
